d8c884a83b950fb5e242044a3000a62ce7c0c5f3
sajjad2014 <sajjad@lisbon.usc.edu>
2025-07-09 19:53:51 -0700

    add preprocessing, spliting, dgsurv, and baselines

---
+++ sub_models/mrna_net.py
@@ -0,0 +1,35 @@
+"""sub_model: mRNA Nerual Network with fully connected layers"""
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class MRNANet(nn.Module):
+	"""Extract representations from mRNA modality"""
+	def __init__(self, mrna_length, m_length):
+		"""miRNA Nerual Network with fully connected layers
+		Parameters
+		----------
+		mrna_length: int
+			The input dimension of mRNA modality.
+
+		m_length: int
+			Output dimension.
+
+		"""
+		super(MRNANet, self).__init__()
+
+		# Linear Layers
+		self.mrna_hidden1 = nn.Linear(mrna_length, m_length)
+		self.bn1 = nn.BatchNorm1d(m_length)
+
+		#Dropout_layer
+		self.dropout_layer2 = nn.Dropout(p=0.4)
+
+	def forward(self, mrna_input):
+		mrna = torch.relu(self.bn1(self.mrna_hidden1(mrna_input)))
+		mrna = self.dropout_layer2(mrna)
+
+		return mrna
